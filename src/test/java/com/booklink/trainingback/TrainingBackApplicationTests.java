2b659a6eb7cb6ef429f935d50623234786db1c1d
TomasBruno1 <tomas.bruno@ing.austral.edu.ar>
2023-08-03 17:26:09 -0300

    Initial commit

---
+++ src/test/java/com/booklink/trainingback/TrainingBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booklink.trainingback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
